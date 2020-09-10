c9647c216c94575c1070da8a4ea38596bb7474ae
Fiifi Pius <fiifipius@gmail.com>
2020-09-10 11:07:55 +0000

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+A = [64, 25, 12, 22, 11] 
+for i in range(len(A)):
+    min_index = i
+    for j in range(i+1, len(A)):
+        if A[min_index] > A[j]:
+            min_index = j
+
+    A[i], A[min_index] = A[min_index], A[i]
+
+print(A)
+
+# time complexity O(n square)
+# space complexity O(1)
